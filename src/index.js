dac3cdfba0cf86df160fcfd96724e6d68a0c4be3
Italiano <mduviafilho@gmail.com>
2023-02-15 17:06:58 +0000

    Update 2 files

--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-// you can write to stdout for debugging purposes, e.g.
-// console.log('this is a debug message');
+let numero = 10;
 
-function solution(A) {
-  // write your code in JavaScript (Node.js 8.9.4)
-  var i = 1;
-  var max = i;
-  var data = {};
-
-  for (var i in A) {
-    data[A[i]] = A[i];
-  }
-
-  console.log(data);
-
-  while (true) {
-    if (!data[i]) {
-      max = i;
-      break;
-    }
-
-    i++;
-  }
-
-  return max;
-}
-
-var d = [];
-
-for (var k = 0; k <= 100000; k++) {
-  d.push(k);
-}
-
-console.log(solution(d));
+console.log(numero);
